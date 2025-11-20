8a08e111012c595d13c845d8327e5800d4f3a559
Carolina Barreiro Calvelo <a23carolinabc@iessanclemente.net>
2025-11-20 12:22:41 +0100

    Cambios program

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-builder.Services.Configure<Configuracion>(builder.Configuration);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -21,7 +20,11 @@ builder.Services.AddScoped<AutenticacionInterceptor>();
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services
     .AddHttpClient("API", client => {
-        client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("BackendUrlApi"));
+        string? backendUrl = builder.Configuration.GetValue<string>("BackendUrlApi");
+        if (!string.IsNullOrEmpty(backendUrl))
+        {
+            client.BaseAddress = new Uri(backendUrl);
+        }
     })
     .AddHttpMessageHandler<AutenticacionInterceptor>();
 builder.Services.AddLocalization(options =>
@@ -37,7 +40,6 @@ builder.Services.AddScoped<ServicioAutentificacion>();
 builder.Services.AddScoped<ServicioUsuarios>();
 builder.Services.AddSingleton<ServicioDatosContexto>();
 builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<ServicioAutentificacion>());
-builder.Services.AddAuthorizationCore();
 builder.Services.AddAuthorizationCore(config =>
 {
     foreach (var permiso in UtilsPermisos.ObtenerTodosLosPermisos())
